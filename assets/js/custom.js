cf03b6226a68e2c413780673bb295b62a41fa0d1
axle <rodriguez.imaxle@gmail.com>
2020-03-27 14:23:22 +0800

    11 commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,3 +7,22 @@ $(window).scroll(function(e) {
     
 });
 
+    $('.navbar-full').click(function(event){
+        window.location.href = "#navbar-full";
+        history.pushState('', document.title, window.location.pathname);
+    });
+
+    $('.about').click(function(event){
+        window.location.href = "#about";
+        history.pushState('', document.title, window.location.pathname);
+    });
+
+    $('.portfolio').click(function(event){
+        window.location.href = "#portfolio";
+        history.pushState('', document.title, window.location.pathname);
+    });
+
+    $('.contact').click(function(event){
+        window.location.href = "#contact";
+        history.pushState('', document.title, window.location.pathname);
+    });
