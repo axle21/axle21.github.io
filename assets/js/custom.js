e69469a7ccbedb880fcf074af8630eb8fd69fb99
axle <rodriguez.imaxle@gmail.com>
2020-05-27 15:22:54 +0800

    minor

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -32,36 +32,39 @@ $(window).scroll(function(e) {
 
 // animation of button HIRE/GEtCV
 
-btnHire.addEventListener("mouseover" , () => {
+if ($(window).width() > 748) {
+    btnHire.addEventListener("mouseover" , () => {
         
-    btnHire.classList.add('magictime', 'puffIn')
-    btnHire.style.boxShadow = '0px 15px 20px gray';
-    btnHire.style.transform = 'translateY(-7px)';
-
-})
-
-btnHire.addEventListener("mouseout" , () => {
-
-    btnHire.classList.remove('magictime' ,'puffIn')
-    btnHire.style.backgroundColor  = '';
-    btnHire.style.boxShadow = '';
-    btnHire.style.transform = '';
-
-})
-
-getCV.addEventListener("mouseover" , () => {
+        btnHire.classList.add('magictime', 'puffIn')
+        btnHire.style.boxShadow = '0px 15px 20px gray';
+        btnHire.style.transform = 'translateY(-7px)';
     
-getCV.classList.add('magictime', 'puffIn')
-getCV.style.boxShadow = '0px 15px 20px gray';
-getCV.style.transform = 'translateY(-7px)';
+    })
+    
+    btnHire.addEventListener("mouseout" , () => {
+    
+        btnHire.classList.remove('magictime' ,'puffIn')
+        btnHire.style.backgroundColor  = '';
+        btnHire.style.boxShadow = '';
+        btnHire.style.transform = '';
+    
+    })
+    
+    getCV.addEventListener("mouseover" , () => {
+        
+    getCV.classList.add('magictime', 'puffIn')
+    getCV.style.boxShadow = '0px 15px 20px gray';
+    getCV.style.transform = 'translateY(-7px)';
+    
+    })
+    
+    getCV.addEventListener("mouseout" , () => {
+    
+    getCV.classList.remove('magictime' ,'puffIn')
+    getCV.style.backgroundColor  = '';
+    getCV.style.boxShadow = '';
+    getCV.style.transform = '';
+    
+    })
+}
 
-})
-
-getCV.addEventListener("mouseout" , () => {
-
-getCV.classList.remove('magictime' ,'puffIn')
-getCV.style.backgroundColor  = '';
-getCV.style.boxShadow = '';
-getCV.style.transform = '';
-
-})
